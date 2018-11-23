0816b34e0c231b833cd207bc4e0961d467a94622
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-11-23 22:06:42 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,5 +1,6 @@
 class CashRegister
   attr_accessor :total, :discount, :items, :price, :quantity, :last_transaction
+  
   def initialize(discount = 0)
     @total = 0
     @discount = discount || 0
