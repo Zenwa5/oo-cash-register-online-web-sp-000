3c6bf3c62b7c1e992419709f75ae382694a86a8b
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-11-23 22:11:17 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,10 +13,21 @@ class CashRegister
        quantity.times do
         @items << item
       end
-      # @items.concat(quantity.times.map{item})
-      # @items.concat([item] * quantity)
+    
     
   end
 
-
+  def apply_discount
+      if @discount > 0
+        discount_percent = @discount / 100.0
+        @total -= (@total * discount_percent)
+        "After the discount, the total comes to $#{total.to_i}."
+      else
+        "There is no discount to apply."
+      end
+    end
+     def void_last_transaction
+      @total -= @last_transaction
+    end
+  end 
 end
